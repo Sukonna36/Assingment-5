8cd211d335bd3960170fabd1bfa2f0b365e57f91
HPelitebook840g3 <sukonna36@gmail.com>
2025-10-28 21:13:59 +0600

    call button function added

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+// heart icon count
 let count = 0;
 const heartCount = document.getElementById('heart-count');
 const hearts = document.getElementsByClassName('heart-icon');
@@ -8,3 +9,33 @@ for(let i=0; i<hearts.length; i++){
     heartCount.innerText = count;
   })
 }
+
+
+
+// call button
+
+  let coins = 100;
+  const coinCount = document.getElementById('coin-count');
+
+ const callButtons = document.getElementsByClassName('call-button')
+  for( let button of callButtons ){
+    button.addEventListener('click', function(){
+      const card = this.parentElement.parentElement.parentElement;
+      const surviceName = card.querySelector('.survice-name').innerText;
+      const surviceNumber = card.querySelector('.survice-number').innerText;
+
+      if(coins >=20){
+        coins -=20;
+        coinCount.innerText = coins;
+      }
+      else{
+        alert("Not Enough Coins")
+        return;
+      }
+      
+      alert("📞 Calling " + surviceName + " " + surviceNumber);
+
+      
+
+    })
+  }
