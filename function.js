117bc3c14f21478867a6216ac3a4c3c1673ca285
HPelitebook840g3 <sukonna36@gmail.com>
2025-10-29 01:42:34 +0600

    added call history feature

--- function.js
+++ function.js
@@ -18,8 +18,14 @@ for(let i=0; i<hearts.length; i++){
 
   let coins = 100;
   const coinCount = document.getElementById('coin-count');
+  const callButtons = document.getElementsByClassName('call-button');
 
- const callButtons = document.getElementsByClassName('call-button')
+  const historyEntries = document.getElementById('history-entries');
+  const clearBtn = document.getElementById('clear-history');
+  clearBtn.addEventListener('click', function(){
+    historyEntries.innerHTML = '';
+  });
+
   for( let button of callButtons ){
     button.addEventListener('click', function(){
       const card = this.parentElement.parentElement.parentElement;
@@ -37,7 +43,21 @@ for(let i=0; i<hearts.length; i++){
       
       alert("📞 Calling " + surviceName + " " + surviceNumber);
 
-      })
+      const historyCard = document.createElement('div');
+      historyCard.classList.add('flex', 'justify-between', 'items-center','text-black', 'bg-[#f2f2f2]','rounded-md', 'py-2', 'pl-3');
+      
+      const left = document.createElement('div');
+      left.innerHTML =
+       `<div class="history-service-name font-semibold">${surviceName}</div>
+        <div class='history-service-number text-gray-600 text-sm'>${surviceNumber}</div>`;
+        
+        const right = document.createElement('div');
+        right.innerText = new Date().toLocaleTimeString();
+        right.classList.add('text-gray-400', 'text-sm'); 
+        historyCard.appendChild(left);
+        historyCard.appendChild(right);
+        historyEntries.appendChild(historyCard);
+      });
   }
 
 
